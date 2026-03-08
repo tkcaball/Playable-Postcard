ee88f983c69d8bd9bdf67e1aae2c6878a43ea95c
ticaballero2 <ticaballero2@gmail.com>
2026-03-07 23:42:24 -0800

    Added audio files and sound effects for interactions with images

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -6,6 +6,10 @@ class Play extends Phaser.Scene {
     preload(){
         this.load.image("jhope", "./assets/Jhope.png")
         this.load.image("crowd", "./assets/Crowd.png")
+        this.load.audio("artist", "./assets/Jhope_audio.mp3")
+        this.load.audio("cheer", "./assets/Cheering.mp3")
+        this.load.audio("music", "./assets/Music.mp3")
+        this.load.audio("lightswitch", "./assets/Lightswitch.mp3")
     }
 
     create() {
@@ -20,11 +24,23 @@ class Play extends Phaser.Scene {
         graphics.fillStyle(0x4444ff, 1)
         graphics.fillRect(220, 40, 380, 150)
 
+        //Sounds
+        this.artistSound = this.sound.add("artist")
+        this.cheerSound = this.sound.add("cheer")
+
         this.jhope = this.add.image(400, 280, "jhope")
         this.jhope.setScale(0.15)
+        this.jhope.setInteractive({ pixelPerfect: true, useHandCursor: true })
+        this.jhope.on("pointerdown", () => {
+            this.artistSound.play()
+        })
 
         this.crowd = this.add.image(400, 160, "crowd")
         this.crowd.setScale(0.70)
+        this.crowd.setInteractive({ pixelPerfect: true, useHandCursor: true })
+        this.crowd.on("pointerdown", () => {
+            this.cheerSound.play()
+        })
 
     }
 

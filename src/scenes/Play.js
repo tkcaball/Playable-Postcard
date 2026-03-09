8b558d2f766cc0eb19a1dda451056364a55e379c
ticaballero2 <ticaballero2@gmail.com>
2026-03-09 13:39:41 -0700

    memory game

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -6,6 +6,7 @@ class Play extends Phaser.Scene {
     preload(){
         this.load.image("jhope", "./assets/Jhope.png")
         this.load.image("crowd", "./assets/Crowd.png")
+
         this.load.audio("artist", "./assets/Jhope_audio.mp3")
         this.load.audio("cheer", "./assets/Cheering.mp3")
         this.load.audio("music", "./assets/Music.mp3")
@@ -18,7 +19,7 @@ class Play extends Phaser.Scene {
 
         //Stage
         graphics.fillStyle(0x606060, 1)
-        graphics.fillRect(200, 240, 400, 180) //(x,y,w,h)
+        graphics.fillRect(200, 240, 400, 180)
 
         //Stage Screen
         graphics.fillStyle(0x4444ff, 1)
@@ -27,6 +28,7 @@ class Play extends Phaser.Scene {
         //Sounds
         this.artistSound = this.sound.add("artist")
         this.cheerSound = this.sound.add("cheer")
+        this.musicSound = this.sound.add("music")
 
         //Stage Light
         this.stageLight = this.add.triangle(485, 150, 0, 0, 80, 500, -80, 500, 0xff0000)
@@ -64,11 +66,94 @@ class Play extends Phaser.Scene {
         //Postcard scene
         this.keyP = this.input.keyboard.addKey('P')
 
+        //Memory game
+        this.keyM = this.input.keyboard.addKey('M')
+        this.keyR = this.input.keyboard.addKey('R')
+        this.keyG = this.input.keyboard.addKey('G')
+        this.keyB = this.input.keyboard.addKey('B')
+
+        this.pattern = ["R","G","B"]
+        this.playerPattern = []
+        this.memoryActive = false
+
+        //Instructions
+        this.add.text(260, 60,
+            "Press M to start memory game\nRepeat with R G B",
+            { font: "18px Helvetica", color: "#ffffff", align: "center" }
+        )
+
     }
 
     update() {
         if(Phaser.Input.Keyboard.JustDown(this.keyP)){
             this.scene.start("postcardScene")
+        }
+
+        if(Phaser.Input.Keyboard.JustDown(this.keyM)) {
+            this.startMemoryGame()
+        }
+
+        //Player input
+        if(this.memoryActive){
+
+            if(Phaser.Input.Keyboard.JustDown(this.keyR)){
+            this.checkPattern("R")  // was playerInput
+            }
+            if(Phaser.Input.Keyboard.JustDown(this.keyG)){
+                this.checkPattern("G")
+            }
+            if(Phaser.Input.Keyboard.JustDown(this.keyB)){
+                this.checkPattern("B")
+            }
+
+        }
+    }
+
+    startMemoryGame() {
+        this.memoryActive = false
+        this.playerPattern = []
+
+        let i = 0
+
+        this.time.addEvent({
+            delay: 700,
+            repeat: this.pattern.length - 1,
+            callback: () => {
+                let key = this.pattern[i]
+
+                if(key === "R") this.stageLight.setFillStyle(0xff0000)
+                if(key === "G") this.stageLight.setFillStyle(0x00ff00)
+                if(key === "B") this.stageLight.setFillStyle(0x0000ff)
+
+                this.time.delayedCall(300, ()=>{
+                    this.stageLight.setFillStyle(0x000000)
+                })
+
+                i++
+
+                if(i == this.pattern.length){
+                    this.memoryActive = true
+                }
+            }
+        })
+    }
+
+    checkPattern(key){
+        this.playerPattern.push(key) 
+        
+        let index = this.playerPattern.length - 1
+        if(this.playerPattern[index] != this.pattern[index]) {
+            console.log("Wrong pattern")
+            this.playerPattern =[]
+            return
+        }
+
+        if(this.playerPattern.length == this.pattern.length){
+            console.log("Correct!")
+            this.musicSound.play()
+            this.time.delayedCall(2000, ()=>{
+                this.scene.start("postcardScene")
+            })
         }
     }
 }
